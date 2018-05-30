d872610616732f5d3de70656fe8b2aa097f5dede
Kartik Mudgal <kartikmudgal1998@gmail.com>
2018-05-30 12:31:07 +0530

    Create basic frontend

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>KARTIK</title>
+
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+	body,h1 {font-family: "Raleway", Arial, sans-serif;}
+	h1 {letter-spacing: 6px;
+			text-align: center;
+			text-shadow: 3px 2px green;
+			}
+	form{
+		border-style: solid;
+		border-color: blue;
+		border-width: 5px;
+		text-align: center;
+	}
+	input{
+		border-style: solid;
+		outline-style: outset;
+    	outline-color: yellow;
+	}
+	table {
+	border-collapse: collapse;
+	border-spacing: 0;
+	border-style: solid;
+}
+
+td,
+th {
+	padding: 5px;
+	border-bottom: 1px solid #aaa;
+}
+	</style>
+</head>
+
+<body>
+	<h1>__CRUDify__</h1><br>
+	<h1 class="w3-xlarge">Kartik Mudgal</h1>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require "header.php";?><br><br>
+	<div class="w3-padding-32" style="background-color: red">
+    <div class="w3-bar w3-border" style="max-width: 1500px;background-color: blue">
+	<a href="create.php" class="w3-bar-item w3-button">ADD A USER</a>
+	<a href="read.php" class="w3-bar-item w3-button">FIND A USER</a>
+	<a href="update.php" class="w3-bar-item w3-button">UPDATE A USER</a>
+	<a href="delete.php" class="w3-bar-item w3-button">DELETE A USER</a>
+</div>
+</div><br><br>
+<div style="text-align: center; padding: 10px 10px 10px 10px;border-style: solid;border-width: 10px;border-color: red;outline-style: outset;outline-color: yellow;">
+<img src = "welcome.jpeg" width = "500" height="300">
+</div>
+<?php require "footer.php" ?>
