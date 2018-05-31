98d83b76c9ae437a4d33fda58aa8f2be0b70ef0f
Kartik Mudgal <kartikmudgal1998@gmail.com>
2018-05-31 16:02:20 +0530

    Add logout hyperlink

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-require "header.php";?><br><br>
+require "header.php" ?><br><br>
 	<div class="w3-padding-32" style="background-color: red">
     <div class="w3-bar w3-border" style="max-width: 1500px;background-color: blue">
 	<a href="create.php" class="w3-bar-item w3-button">ADD A USER</a>
 	<a href="read.php" class="w3-bar-item w3-button">FIND A USER</a>
 	<a href="update.php" class="w3-bar-item w3-button">UPDATE A USER</a>
 	<a href="delete.php" class="w3-bar-item w3-button">DELETE A USER</a>
+	<a href="logout.php" class="w3-bar-item w3-button">LOG OUT</a>
+
 </div>
 </div><br><br>
 <div style="text-align: center; padding: 10px 10px 10px 10px;border-style: solid;border-width: 10px;border-color: red;outline-style: outset;outline-color: yellow;">
